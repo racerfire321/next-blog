9a9684fd7062aec805790d051a6e85c3557d963f
racerfire321 <racerfire321@gmail.com>
2023-07-12 11:39:20 +0545

    setup header and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import NavBar from '@/component/NavBar'
+import Footer from '@/component/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar/>
+        {children}
+          <Footer/>
+        </body>
     </html>
   )
 }
