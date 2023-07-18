c2ce4672405393527c1f29e9f3b592364e8b2e76
racerfire321 <racerfire321@gmail.com>
2023-07-18 11:53:27 +0545

    adding style to footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import styles from "./footer.module.css";
+
 import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
+    <div class="h-16 text-sm flex items-center justify-between">
       <div>©2023 Lamamia. All rights reserved.</div>
-      <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+      <div class="flex items-center gap-2">
+        <Image src="/1.png" width={15} height={15} class="opacity-60 cursor-pointer" alt="Lama Dev Facebook Account" />
+        <Image src="/2.png" width={15} height={15} class="opacity-60 cursor-pointer" alt="Lama Dev" />
+        <Image src="/3.png" width={15} height={15} class="opacity-60 cursor-pointer"alt="Lama Dev" />
+        <Image src="/4.png" width={15} height={15} class="opacity-60 cursor-pointer"alt="Lama Dev" />
       </div>
     </div>
   );
