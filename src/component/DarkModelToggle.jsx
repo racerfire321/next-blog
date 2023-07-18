bb34035d9d951edfc7b35374de308f781ef298b2
racerfire321 <racerfire321@gmail.com>
2023-07-18 12:05:49 +0545

    adding style on darkmodeltoggle

--- src/component/DarkModelToggle.jsx
+++ src/component/DarkModelToggle.jsx
@@ -1,17 +1,16 @@
 "use client"
 
 import React, { useContext } from "react";
-import styles from "./darkModeToggle.module.css";
 import { ThemeContext } from "../../context/ThemeContext";
 
 const DarkModeToggle = () => {
   const { toggle,mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container} onClick={toggle}>
-      <div className={styles.icon}>🌙</div>
-      <div className={styles.icon}>🔆</div>
+    <div class="h-42 h-24 boredr-1.5 border-solid border-green-500 rounded-30 flex items-center justify-between p-2 relative cursor-pointer " onClick={toggle}>
+      <div class="text-12">🌙</div>
+      <div class="text-12">🔆</div>
       <div
-        className={styles.ball}
+        class="ball w-15 h-15 bg-green-500 rounded-full absolute"
         style={mode === "light" ? { left: "2px" } : { right: "2px" }}
       />
     </div>
