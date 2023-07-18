9329007a8554b43fee67cb8a89e8caefeb27c236
racerfire321 <racerfire321@gmail.com>
2023-07-18 12:07:32 +0545

    adding style on button

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Button = ({ text, url }) => {
   return (
     <Link href={url}>
-      <button className={styles.container}>{text}</button>
+      <button class="container p-20 cursor-pointer bg-green-500 rounded-5 text-white">{text}</button>
     </Link>
   );
 };
